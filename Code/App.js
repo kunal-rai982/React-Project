8c63e78d0461e30fb1f403790bcf92b3f02ee5ad
kunal-rai <kunalrai982@gmail.com>
2023-01-26 13:18:07 +0530

    Chapter 1 Code

---
+++ Code/App.js
@@ -0,0 +1,12 @@
+const head = React.createElement("h1", {}, "namaste");
+
+const head2 = React.createElement("h3", { 
+    props: "attribute" 
+}, "React");
+
+const container = React.createElement("div", { id: "container" }, [
+    head,
+    head2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
